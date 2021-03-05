988b7f50a1025d5f2ec8cc5a015b1a8df507b537
David M <PieKing1215@users.noreply.github.com>
2021-03-04 20:40:39 -0500

    Config file & shift the main menu time up a couple pixels

--- src/main/java/me/pieking1215/startupqol/StartupQoL.java
+++ src/main/java/me/pieking1215/startupqol/StartupQoL.java
@@ -44,6 +44,7 @@ public class StartupQoL {
 
     static {
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+            StartupQoLConfig.loadConfig();
             expectedTime = StartupQoLConfig.getTimeEstimate();
         });
     }
@@ -87,7 +88,7 @@ public class StartupQoL {
             if(guiScale <= 0) guiScale = 1; // failsafe to prevent divide by 0
 
             String txt = "Startup took " + minutes + "m " + seconds + "s.";
-            Minecraft.getInstance().fontRenderer.drawStringWithShadow(new MatrixStack(), txt, Minecraft.getInstance().getMainWindow().getWidth()/2 / guiScale - Minecraft.getInstance().fontRenderer.getStringWidth(txt)/2, Minecraft.getInstance().getMainWindow().getHeight() / guiScale - 16, Color.YELLOW.getRGB());
+            Minecraft.getInstance().fontRenderer.drawStringWithShadow(new MatrixStack(), txt, Minecraft.getInstance().getMainWindow().getWidth()/2 / guiScale - Minecraft.getInstance().fontRenderer.getStringWidth(txt)/2, Minecraft.getInstance().getMainWindow().getHeight() / guiScale - 20, Color.YELLOW.getRGB());
         }else if(hasBeenMainMenu){
             hasLeftMainMenu = true;
         }
--- src/main/java/me/pieking1215/startupqol/StartupQoLConfig.java
+++ src/main/java/me/pieking1215/startupqol/StartupQoLConfig.java
@@ -15,15 +15,80 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class StartupQoLConfig {
+
+    // the initial values here are used as defaults
     public static float fadeOutTime = 1000.0f;
     public static float fadeInTime = 500.0f;
     public static boolean timerOnTop = false;
 
     public static final File DOT_MINECRAFT = FMLPaths.GAMEDIR.get().resolve(FMLConfig.defaultConfigPath()).toFile().getParentFile();
     public static final File TIMES_FILE = new File(DOT_MINECRAFT, "config/startupQoL/startup_times.json");
+    public static final File CONFIG_FILE = new File(DOT_MINECRAFT, "config/startupQoL/config.json");
+
+    public static final String KEY_TIMER_ON_TOP = "timerOnTop";
+    public static final String KEY_FADE_OUT_TIME = "fadeOutTimeMs";
+    public static final String KEY_FADE_IN_TIME = "fadeInTimeMs";
+
+    public static void loadConfig(){
+        if(CONFIG_FILE.exists()) {
+            try {
+                JsonReader jr = new JsonReader(new FileReader(CONFIG_FILE));
+                JsonElement jp = new JsonParser().parse(jr);
+                if (jp.isJsonObject()) {
+                    JsonObject obj = jp.getAsJsonObject();
+
+                    if (obj.has(KEY_TIMER_ON_TOP)) {
+                        StartupQoLConfig.timerOnTop = obj.get(KEY_TIMER_ON_TOP).getAsBoolean();
+                    }
+
+                    if(obj.has(KEY_FADE_OUT_TIME)){
+                        StartupQoLConfig.fadeOutTime = obj.get(KEY_FADE_OUT_TIME).getAsFloat();
+                    }
+
+                    if(obj.has(KEY_FADE_IN_TIME)){
+                        StartupQoLConfig.fadeInTime = obj.get(KEY_FADE_IN_TIME).getAsFloat();
+                    }
+
+                }
+                jr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        saveConfig();
+    }
+
+    public static void saveConfig(){
+        try{
+            CONFIG_FILE.getParentFile().mkdirs();
+            if(!CONFIG_FILE.exists()) CONFIG_FILE.createNewFile();
+
+            JsonWriter jw = new JsonWriter(new FileWriter(CONFIG_FILE));
+            jw.setIndent("  ");
+            jw.beginObject();
+
+            jw.name(KEY_TIMER_ON_TOP).value(timerOnTop);
+            jw.name(KEY_FADE_OUT_TIME).value(fadeOutTime);
+            jw.name(KEY_FADE_IN_TIME).value(fadeInTime);
+
+            jw.endObject();
+            jw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
     public static long getTimeEstimate(){
         // try to load previous times
+        // times file format
+        // {
+        //   "times": [
+        //     34784,
+        //     34726,
+        //     36204
+        //   ]
+        // }
         try {
             TIMES_FILE.getParentFile().mkdirs();
             if(!TIMES_FILE.exists()) TIMES_FILE.createNewFile();
